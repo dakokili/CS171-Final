8b72c680860860adf102f2a838c66d511ca63736
dakokili <60498897+dakokili@users.noreply.github.com>
2022-12-17 19:40:35 +0800

    finished dirty deposition works

--- include/defines.h
+++ include/defines.h
@@ -1,10 +1,27 @@
 #ifndef _DEFINES_H_
 #define _DEFINES_H_
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <openvdb/openvdb.h>
 
 #include <iostream>
 #include <cassert>
+
+template <typename T, int size>
+using Vec = Eigen::Matrix<T, size, 1>;
+
+using Vec3f = Eigen::Vector3f;
+using Vec3i = Eigen::Vector3i;
+using Vec2f = Eigen::Vector2f;
+using Vec2i = Eigen::Vector2i;
+using Vec4f = Eigen::Vector4f;
+using Mat3f = Eigen::Matrix3f;
+
+constexpr float PI = 3.141592653579f;
+constexpr float INV_PI = 0.31830988618379067154;
 
 #define LOG_INFO(msg)                             \
   std::cerr << "[INFO] "                          \
@@ -24,4 +41,5 @@
     std::cerr << "not implemented" << std::endl; \
     assert(false);                               \
   } while (0)
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,6 @@
 #include <utils.h>
-const int WIDTH = 720;
-const int HEIGHT = 720;
-GLFWwindow *window;
-
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-} 
-
-
-int main() {
-    WindowGuard windowguard(window, WIDTH, HEIGHT, "CS171 Final");
-
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glEnable(GL_DEPTH_TEST);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        processInput(window);
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    }
+int main(int argc, char* argv[]){
+    openvdb::io::File file(getPath("assets/single-res small.vdb",5));
+    std::cout<<file.filename();
     return 0;
 }
