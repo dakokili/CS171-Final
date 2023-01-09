2632bd04f73b9e6e88b4047ceed221d15c815f99
dakokili <60498897+dakokili@users.noreply.github.com>
2023-01-09 18:56:24 +0800

    add some switches

--- src/camera.cpp
+++ src/camera.cpp
@@ -82,8 +82,6 @@ void Camera::shotimage(Scene& scene){
                 ans+=transferfunction(interactions);
             }
             image.setpixel(j,image.resolution.y()-1-i,ans/SPP);
-            //scene.intersect(generateray(float(j)+0.5f,float(i)+0.5f),interactions);
-            //image.setpixel(j,image.resolution.y()-1-i,transferfunction(interactions));
         }
     }
     image.writefile("result.png");
@@ -98,56 +96,53 @@ openvdb::math::Ray<float> Camera::generateray(float x,float y){
 
 Vec3i Camera::transferfunction(std::vector<Interaction>& interactions){
     if(interactions.size()==0) return{0,0,0};
-    
     else
     {
-        int length = interactions.size();
-        Vec3f s={0,0,0};
-        float t=.1f;
-        int count=0;
-        bool objhit=false;
-        auto _ratio= [](int counter,float t){
-            auto temp=0.0f;
-            for(int i=0;i<counter;i++) temp=temp*(1-t)+t;
-            return 1.f/temp;
-        };
-        for(int i=0 ; i < length ; i++)
-        {
-            if(interactions[i].type==Interaction::VOXEL){
-                interactions[i].value*=4800.f;
-                if(interactions[i].value>1.f){
-                    count+=interactions[i].scale/0.008f;
-                    for(int k=0;k<interactions[i].scale/0.008f;k++){
-                        s*=1-t;
-                        s+=t*Vec3f(1.f/powf(interactions[i].value,.3f),1.f-1.f/powf(interactions[i].value,.3f),0);
+        if(!velocity){
+            int length = interactions.size();
+            Vec3f s={0,0,0};
+            float t=transparency;
+            int count=0;
+            bool objhit=false;
+            auto _ratio= [](int counter,float t){
+                auto temp=0.0f;
+                for(int i=0;i<counter;i++) temp=temp*(1-t)+t;
+                return 1.f/temp;
+            };
+            for(int i=0 ; i < length ; i++)
+            {
+                if(interactions[i].type==Interaction::VOXEL){
+                    interactions[i].value*=4800.f;
+                    if(interactions[i].value>1.f){
+                        count+=interactions[i].scale/0.008f;
+                        for(int k=0;k<interactions[i].scale/0.008f;k++){
+                            s*=1-t;
+                            s+=t*Vec3f(1.f/powf(interactions[i].value,.3f),1.f-1.f/powf(interactions[i].value,.3f),0);
+                        }
                     }
                 }
-            }
-            else if(interactions[i].type==Interaction::GEOMETRY){
-                if(count>0){
-                    s*=_ratio(count,t);
+                else if(interactions[i].type==Interaction::GEOMETRY){
+                    if(count>0){
+                        s*=_ratio(count,t);
+                    }
+                    else s+=Vec3f{.8f,.3f,.8f}*(.4f+.2f*std::max(0.f,interactions[i].normal.dot(Vec3f(0,10,0)-interactions[i].pos)));
+                    objhit=true;
+                    break;
                 }
-                else s+=Vec3f{.8f,.3f,.8f}*(.4f+.2f*std::max(0.f,interactions[i].normal.dot(Vec3f(0,10,0)-interactions[i].pos)));
-                objhit=true;
-                break;
+                
             }
-            
+            if(!objhit&&count>0) s*=_ratio(count,t);
+            return converttoRGB(s);
         }
-        if(!objhit&&count>0) s*=_ratio(count,t);
-        return converttoRGB(s);
-    }
-    
-    /*
-    else
-    {
-        int length = interactions.size();
-        Vec3f radiance={0,0,0};
-        Vec3f s={0,0,0};
-        float t=.2f;
-        bool bound=false;
-        for(int i=0 ; i < length ; i++)
-        {
-            if(interactions[i].type==Interaction::VOXEL){
+        else{
+            int length = interactions.size();
+            Vec3f radiance={0,0,0};
+            Vec3f s={0,0,0};
+            float t=transparency;
+            bool bound=false;
+            for(int i=0 ; i < length ; i++)
+            {
+                if(interactions[i].type==Interaction::VOXEL){
                     if(interactions[i].value<1.9f/30.f&&interactions[i].value>1.8f/30.f){
                         s*=1-t;
                         s+=t*Vec3f((interactions[i].value*30.0f-1.8f)/0.2f+0.5f,(1.9f-interactions[i].value*30.0f)/0.2f,0);
@@ -160,13 +155,13 @@ Vec3i Camera::transferfunction(std::vector<Interaction>& interactions){
                         s*=1-t;
                         s+=t*Vec3f(0,interactions[i].value*30.0f/1.5f,(1.5f-interactions[i].value*30.0f)/1.5f);
                     }
+                }
+                else if(interactions[i].type==Interaction::GEOMETRY){
+                    s+=Vec3f{.8f,.3f,.8f}*(.4f+.2f*std::max(0.f,interactions[i].normal.dot(Vec3f(0,10,0)-interactions[i].pos)));
+                    break;
+                }
             }
-            else if(interactions[i].type==Interaction::GEOMETRY){
-                s+=Vec3f{.8f,.3f,.8f}*(.4f+.2f*std::max(0.f,interactions[i].normal.dot(Vec3f(0,10,0)-interactions[i].pos)));
-                break;
-            }
+            return converttoRGB(s);
         }
-        return converttoRGB(s);
     }
-    */
 }
